d5f9ceb2da2703138e02a1bc38a1981049c87f08
Khalifa <detective.khalifah.dtcfscd@gmail.com>
2023-02-21 13:25:19 -0800

    Use 'else' branching statements instead of IFs only

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - prints position of a number on the number line;
  * positive, negative, or neutral.
- * @n: an int.
+ * @n: an int - the number to be checked.
  * Return: 1 if positive;
  * 0 if neutral;
  * -1 if negative.
@@ -15,13 +15,15 @@ int print_sign(int n)
 		_putchar('-');
 		return (-1);
 	}
-	if (n > 0)
+	else if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-
+	else
+	{
 	_putchar('0');
 	return (0);
+	}
 }
 
