14d2c11dded1610141cd265270330ea7d211fc3a
Khalifa <detective.khalifah.dtcfscd@gmail.com>
2023-02-21 06:04:52 -0800

    Add program 0-putchar.c; add readme file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <_putchar.c>
+
+/**
+ * main - Prints '_putchar', followed by a newline.
+ * Return: 0 if successful.
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
+
