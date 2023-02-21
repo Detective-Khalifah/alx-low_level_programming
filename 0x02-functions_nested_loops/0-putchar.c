9d4307197a14169ec70c15d6b87b26ab47025b02
Khalifa <detective.khalifah.dtcfscd@gmail.com>
2023-02-21 12:21:32 -0800

    fixup commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * main - Prints '_putchar', followed by a newline.
@@ -14,6 +14,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,4 +1,5 @@
-## Scripts
+## 0x02. C - Functions, nested loops
+### Scripts
 - main.h: header file; comprises _putchar function.
 - 0-putchar.c: prints '_putchar' followed by a newline.
 
