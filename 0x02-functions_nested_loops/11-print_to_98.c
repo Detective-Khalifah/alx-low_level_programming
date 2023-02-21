cbb075d5564e55124d00bbd8468353312fc2d3eb
Khalifa <detective.khalifah.dtcfscd@gmail.com>
2023-02-21 15:32:53 -0800

    Add program 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98,
+ * followed by newline.
+ * @n: the passed starting point of count.
+ */
+void print_to_98(int n)
+{
+	while (n <= 98 || n >= 98)
+	{
+		if (n == 98)
+		{
+			_putchar('\n');
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+			break;
+		}
+
+		if (n < 98)
+		{
+			if (n > 9)
+				_putchar(n / 10 + '0');
+
+			_putchar(n % 10 + '0');
+			n++;
+		}
+
+		if (n > 98)
+		{
+			if (n > 9)
+				_putchar(n / 10 + '0');
+
+			_putchar(n % 10 + '0');
+			n--;
+		}
+
+		_putchar(',');
+		_putchar(' ');
+
+}
+
