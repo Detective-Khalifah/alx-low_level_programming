d0364610d5706a9f6e2c9706eda684c846f11b66
Khalifa <detective.khalifah.dtcfscd@gmail.com>
2023-02-21 12:29:24 -0800

    Update 1-alphabet.c & README.md

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet, in lowercase, followed
+ * by a newline.
+ * Return: 0 if successful.
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c)'
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
+
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -2,4 +2,6 @@
 ### Scripts
 - main.h: header file; comprises _putchar function.
 - 0-putchar.c: prints '_putchar' followed by a newline.
+- 1-alphabet.c: prints the alpahbet (in lowercase), followed by a newline.
+  - 1-main.c: unit test for 1-alphabet.c.
 
