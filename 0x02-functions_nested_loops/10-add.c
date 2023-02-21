9f5f40db54c49fe1487e74bc644df4e922dd3277
Khalifa <detective.khalifah.dtcfscd@gmail.com>
2023-02-21 15:16:25 -0800

    Add program 10-add.c; update main.h with named parameters for the prototype func int add (int, int);

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result.
+ * @a: first int number
+ * @b: second int number
+ * Return: the result of binary operation a + b.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
