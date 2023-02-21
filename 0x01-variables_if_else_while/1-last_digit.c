dff8cad55f8d7478ccb6171adced914898d17126
Khalifa <detective.khalifah.dtcfscd@gmail.com>
2023-02-21 14:29:26 -0800

    delete 1-last_digit (0x01-variables_if_else_while); code program 1-alphabet.c (0x01-variables_if_else_while); fixup 9-times_table.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - computes the last digit of an int, and prints it,
+ * followed by:
+ * "and is greater than 5", if it is greater than 5;
+ * "and is 0", if it is equal to 0;
+ * "and is less than 6 and not 0", if it is between
+ * 6 and 0.
+ * Return: 0 if successful.
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	int lastDigit = n % 10;
+
+	if (lastDigit > 5)
+		printf("%s %d is %d %s", "Last digit of", n, lastDigit,
+				"and is greater than 5");
+	if (lastDigit == 0)
+		printf("%s %d is %d %s", "Last digit of", n, lastDigit,
+				"and is 0");
+	if (lastDigit < 6 && lastDigit != 0)
+		printf("%s %d is %d %s", "Last digit of", n, lastDigit,
+				"and is less than 6 and is not 0");
+
+	return (0);
+}
+
