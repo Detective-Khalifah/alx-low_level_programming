15458c4949890716ec30178b9bee04f207e9c1f7
Khalifa <detective.khalifah.dtcfscd@gmail.com>
2023-02-23 14:03:10 -0800

    Remove print statements; comply with @betty

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,23 +2,14 @@
 
 /**
  * _isupper - checks for uppercase char.
+ * @c: the char to be checked.
  * Return: 1 if the (ASCII) character is uppercase;
  * 0 otherwise.
  */
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
-	{
-		_putchar(c + '0');
-		_putchar(':');
-		_putchar(' ');
-		_putchar(49);
 		return (1);
-	}
-	_putchar(c + '0');
-	_putchar(':');
-	_putchar(' ');
-	_putchar(48);
 	return (0);
 }
 
