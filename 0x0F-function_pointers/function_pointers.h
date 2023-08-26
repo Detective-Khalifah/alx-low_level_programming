86eb601cd2ba4987ddd9b5588fdb94b78edf62ef
Khalifa <detective.khalifah.dtcfscd@gmail.com>
2023-08-26 20:54:04 +0100

    correct 3-get_op_ func.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _FUNCTION_POINTERS_
+#define _FUNCTION_POINTERS_
 
-#include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
