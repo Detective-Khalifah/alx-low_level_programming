2dc20870f161b48d3ac70861da50f87d67af7b99
Khalifa <detective.khalifah.dtcfscd@gmail.com>
2023-08-26 22:20:47 +0100

    remove '*' in func prototype for 1-swap.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,9 +2,10 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <unistd.h>
 
 int _putchar(char c);
-void swap_int(int *a, int *b);
+void swap_int(int a, int b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
