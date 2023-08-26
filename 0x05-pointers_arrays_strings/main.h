1e08b415e0b2b67484812bc43549b95526304c95
Khalifa <detective.khalifah.dtcfscd@gmail.com>
2023-08-26 22:30:56 +0100

    add missing prototype for 1-swap.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,7 +5,8 @@
 #include <unistd.h>
 
 int _putchar(char c);
-void swap_int(int a, int b);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
